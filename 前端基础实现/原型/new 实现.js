d9e46ff55886792d9bb779f1479d8ccbe6ed6488
yanrongbao <yanrongbao1566@163.com>
2019-12-04 22:55:49 +0800

    new 实现

--- 前端基础实现/原型/new 实现.js
+++ 前端基础实现/原型/new 实现.js
@@ -3,7 +3,7 @@
 // 执行构造函数中的代码（为这个新对象添加属性）
 // 返回新对象。
 // 因为 new 没办法重写，我们使用 myNew 函数来模拟 new
-function myNew () {
+function myNew() {
     // 创建一个实例对象
     var obj = new Object();
     // 取得外部传入的构造器
@@ -15,4 +15,13 @@ function myNew () {
     // 如果构造函数返回值是对象则返回这个对象，如果不是对象则返回新的实例对象
     return typeof ret === 'object' && ret !== null ? ret : obj;
 
+}
+
+function objectFactory() {
+    var obj = new Object();
+    var Constructor = [].prototype.unshift(arguments);
+    obj.__proto__ = Constructor.prototype;
+    var ret = Constructor.apply(obj, arguments);
+
+    return typeof ret === 'object' ? ret : obj;
 }
