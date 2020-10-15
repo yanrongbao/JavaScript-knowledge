711969d0770256167beb947d724bd2b38013b71d
yanrongbao <49897696+yanrongbao@users.noreply.github.com>
2020-10-15 13:34:44 +0800

    Update 深度-广度优先遍历.js

--- 前端基础实现/对象/深度-广度优先遍历.js
+++ 前端基础实现/对象/深度-广度优先遍历.js
@@ -3,8 +3,10 @@ let deepTraversal1 = (node, nodeList = []) => {
     if (node !== null) {
         nodeList.push(node);
         let children = node.children;
-        for (let i = 0; i < children.length; i++) {
-            deepTraversal1(children[i], nodeList)
+        if(children) {
+            for (let i = 0; i < children.length; i++) {
+                deepTraversal1(children[i], nodeList)
+            }
         }
     }
     return nodeList;
