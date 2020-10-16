a5e23dc1c23d9231c2c2891ed583a4ba14552095
yanrongbao <49897696+yanrongbao@users.noreply.github.com>
2020-10-16 17:11:11 +0800

    Delete 自定义new.js

--- 前端基础实现/自定义new.js
+++
@@ -1,5 +0,0 @@
-function _new(origin,...arg) {
-  const obj = Object.create(origin.prototype);
-  const result = origin.apply(obj, arg);
-  return Object.prototype.toString.call(result) == '[object Object]'? result : obj;
-}
